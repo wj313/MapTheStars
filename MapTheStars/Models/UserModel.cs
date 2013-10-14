351098108941ed8585b3cb1b71e2cd3d22441beb
lukewestby <lwestby@nd.edu>
2013-10-14 18:20:37 -0400

    Added UserModel class and UsersContext class for database access

---
+++ MapTheStars/Models/UserModel.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="UserModel.cs" company="MapTheStars">
+// Copyright (c) 2013 All Rights Reserved
+// </copyright>
+// <author>Sean Cogan</author>
+// <author>Waleed Johnson</author>
+// <author>Kevin Sonnen</author>
+// <author>Luke Westby</author>
+
+namespace MapTheStars.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    /// <summary>
+    /// Represents a user of the site
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// Primary Key identifier
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// User's public email address
+        /// </summary>
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User's public username
+        /// </summary>
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User's password
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// User's private email to retrieve a Gravatar profile picture
+        /// </summary>
+        [DataType(DataType.EmailAddress)]
+        public string GravatarEmail { get; set; }
+    }
+}
---
+++ MapTheStars/Models/UsersContext.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="UsersContext.cs" company="MapTheStars">
+// Copyright (c) 2013 All Rights Reserved
+// </copyright>
+// <author>Sean Cogan</author>
+// <author>Waleed Johnson</author>
+// <author>Kevin Sonnen</author>
+// <author>Luke Westby</author>
+
+namespace MapTheStars.Models
+{
+    using System;
+    using System.Data.Entity;
+
+    /// <summary>
+    /// Serves the Users table from the database
+    /// </summary>
+    public class UsersContext : DbContext
+    {
+        /// <summary>
+        /// Gets or sets the Users database set from which UserItem models are served
+        /// </summary>
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
