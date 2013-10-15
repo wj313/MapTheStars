47212295f1eb11821e8e60274b9d47e2749893f6
lukewestby <lwestby@nd.edu>
2013-10-15 13:11:22 -0400

    Added UserModelExtensions and corresponding test suite with an extension method to generate a correct url to retrieve a Gravatar profile picture for a user. Also moved solution back from .NET 4.5.1 to .NET 4.5 because of an associated VS2012 bug

---
+++ MapTheStars.Tests/Models/UserModelExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace MapTheStars.Tests.Models
+{
+    using System;
+    using MapTheStars.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UserModelExtensionsTests
+    {
+        private TestContext testContext;
+        public TestContext TestContext
+        {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+
+        [TestMethod]
+        public void TestGenerateGravatarUrl()
+        {
+            TestContext.WriteLine("TestGenerateGravatarUrl ----------------------------------------------");
+            const string testEmail = "test@test.com";
+            const string expectedUrl = "http://www.gravatar.com/avatar/b642b4217b34b1e8d3bd915fc65c4452.jpg";
+            var testUser = new UserModel() { GravatarEmail = testEmail };
+            var actualUrl = testUser.GenerateGravatarUrl();
+            Assert.AreEqual<string>(expectedUrl, actualUrl, "- Generated url should be equal to the one generated and verified online");
+        }
+    }
+}
---
+++ MapTheStars/Models/UserModelExtensions.cs
@@ -0,0 +1,66 @@
+﻿// <copyright file="UserModelExtensions.cs" company="MapTheStars">
+// Copyright (c) 2013 All Rights Reserved
+// </copyright>
+// <author>Sean Cogan</author>
+// <author>Waleed Johnson</author>
+// <author>Kevin Sonnen</author>
+// <author>Luke Westby</author>
+
+namespace MapTheStars.Models
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+    using MapTheStars.Models;
+
+    /// <summary>
+    /// Implements various static extension methods to call from an instance
+    /// of the UserModel class
+    /// </summary>
+    public static class UserModelExtensions
+    {
+        /// <summary>
+        /// Base url for Gravatar image requests
+        /// </summary>
+        private const string gravatarBaseUrl = "http://www.gravatar.com/avatar/";
+
+        /// <summary>
+        /// Image file extension for use in Gravatar image requests
+        /// </summary>
+        private const string gravatarExtension = ".jpg";
+
+        /// <summary>
+        /// Returns a fully formed default Gravatar request url based on the
+        /// user's GravatarEmail field. If the field is null, "" is used and
+        /// it is up to the client to set a default param in the query string
+        /// </summary>
+        /// <param name="user">The calling instance of UserModel</param>
+        /// <returns>The full generated Gravatar url</returns>
+        public static string GenerateGravatarUrl(this UserModel user)
+        {
+            // work on user's GravatarEmail or the empty string if not set
+            var emailString = user.GravatarEmail ?? string.Empty;
+            
+            // clean and lower working string
+            var cleanedString = emailString.Trim().ToLowerInvariant();
+
+            // hash the cleaned string
+            var md5Hasher = MD5.Create();
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(cleanedString));
+
+            // build a lowercase hex string from the hash value
+            var sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            var completedHash = sBuilder.ToString();
+
+            // piece the final result together
+            var generatedUrl = gravatarBaseUrl + completedHash + gravatarExtension;
+            return generatedUrl;
+        }
+    }
+}
